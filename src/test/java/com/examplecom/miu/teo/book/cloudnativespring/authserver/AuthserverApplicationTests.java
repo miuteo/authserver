474122de798503fb98cea99cdca48f1cc06f6f85
teomiu <miu.teo@gmail.com>
2025-06-08 23:52:11 +0300

    Initial commit

---
+++ src/test/java/com/examplecom/miu/teo/book/cloudnativespring/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplecom.miu.teo.book.cloudnativespring.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
